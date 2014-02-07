d275bf37e7b04c44cad8efa0d179d8dedaace8e6
vickiedam <Vickie_90@hotmail.com>
2014-02-07 11:51:03 +0100

    more update for gameEngine

--- Battleship/src/Game/GameEngine.java
+++ Battleship/src/Game/GameEngine.java
@@ -3,9 +3,29 @@ import GUI.Gui;
 
 public class GameEngine {
 
-	//Creates a new object of GUI (for creating a frame)
-	public static void main (String[] args){
+	public static void main(String[] args) {
+		// Creates a new object of GUI (for creating a frame)
 		Gui gui = new GUI.Gui();
+
+		//TODO:
+		
+		// GameEngine kontrollerar players hasShip och anropar gameOver när en
+		// av spelarna inte har några skepp kvar
+
+		// Keeps track of turns of the players
+
 	}
-	
+
+	// keeps the game running
+	public void run() {
+	}
+
+	// starts the game
+	public void newGame() {
+	}
+
+	// ends the game and pops a prompt to the player
+	private void gameOver() {
+	}
+
 }
