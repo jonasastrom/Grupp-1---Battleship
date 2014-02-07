ce3f40fdf7a5577cfe0af2f85f779159b042bc40
vickiedam <Vickie_90@hotmail.com>
2014-02-07 10:43:52 +0100

    main created

--- Battleship/src/Game/GameEngine.java
+++ Battleship/src/Game/GameEngine.java
@@ -1,12 +1,12 @@
 package Game;
-
 import GUI.Gui;
-
 
 public class GameEngine {
 	
+	public static void main (String[] args){
 	Gui gui = new GUI.Gui();
 	
+	}
 	int blargh() {
 		return 9001;
 	}
