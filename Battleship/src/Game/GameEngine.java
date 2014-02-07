737dccdd12f85eafb1978b691fe3e0d662f19508
vickiedam <Vickie_90@hotmail.com>
2014-02-07 10:50:50 +0100

    polished main

--- Battleship/src/Game/GameEngine.java
+++ Battleship/src/Game/GameEngine.java
@@ -2,18 +2,10 @@ package Game;
 import GUI.Gui;
 
 public class GameEngine {
-	
-	public static void main (String[] args){
-	Gui gui = new GUI.Gui();
-	
-	
-	}
-	int blargh() {
-		return 9001;
-	}
-	
-	void printcrap() {
-		System.out.println(blargh());
-	}
 
+	//Creates a new object of GUI (for creating a frame)
+	public static void main (String[] args){
+		Gui gui = new GUI.Gui();
+	}
+	
 }
