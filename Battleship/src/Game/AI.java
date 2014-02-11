c31a2d6d7f307465c4f3ba6ea5e2e45a924498af
Isca <steffiidaniell@gmail.com>
2014-02-11 16:17:24 +0100

    without int difficulties

--- Battleship/src/Game/AI.java
+++ Battleship/src/Game/AI.java
@@ -5,34 +5,46 @@ import java.awt.*;
 
 /**
  * 
- * @author Grupp 1 
+ * @author Grupp 1
  * 
  */
 public class AI extends Player {
 	int difficulties;
-	
+
 	/**
 	 * the constructor of this class
 	 */
-	public AI(int difficulties){
-		this.difficulties = difficulties;
+	public AI() {
+		//this.difficulties = difficulties;
 	}
-	
+
 	@Override
 	/**
 	 * Placing out the ships and 
 	 * it will override the Player class method placeShips() 
 	 * steff does this
 	 */
-	public void placeShips(int ship){
-		
-		//super.placeShips();
+	public void placeShips(int ship) {
+	//	if (isLegalMove(ship) == true) {
+			// place the ship
+			// super.placeShips();
+		//} 
+	//	else
+		//	System.out.println("Try again!");
 	}
-	
-	private boolean ifShips(int ship){
-		
-			return false;
-		
-	}
-	
+
+	/**
+	 * help method for placing out the ships
+	 * @param ship
+	 * @return true if there is no boat there
+	 * @return false if there is a boat there
+	 */
+	//private boolean isLegalMove(int ship){
+		//if ((ship == 3) && Math.abs( )) // if there is no ship on the cell then return false so
+						// you can put it there.
+			// true;
+		//else
+		//	return false;
+	//}
+
 }
