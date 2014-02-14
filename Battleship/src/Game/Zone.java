dffd080b2d4bbdf78c0d4100e9c84e9344150731
medevag <michaeledevag@gmail.com>
2014-02-14 09:31:31 +0100

    new

--- Battleship/src/Game/Zone.java
+++ Battleship/src/Game/Zone.java
@@ -2,4 +2,36 @@ package Game;
 
 public class Zone {
 
+	public zone(int xPos, int yPos)
+	{
+		
+	}
+	
+	
+	
+	public boolean isBombed()
+	{
+		return false;
+	}
+	
+	public boolean setBomb()
+	{
+		return false;
+	}
+	
+	public booelan hasShip()
+	{
+		return false;
+	}
+	
+	/*public Ship getShip()
+	{
+		
+	}*/
+	
+	public void setShip(Ship ship)
+	{
+		
+	}
+	
 }
