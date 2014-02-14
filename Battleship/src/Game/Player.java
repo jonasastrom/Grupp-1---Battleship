594560d7e5c335e18ab206af4f908349c228e457
Magnus <M92@users.noreply.github.com>
2014-02-14 11:12:06 +0100

    new

--- Battleship/src/Game/Player.java
+++ Battleship/src/Game/Player.java
@@ -1,19 +1,67 @@
-package Game;
+package game;
 
-public class Player {
-	
-	public boolean hasShips(){
-		return false;
-	}
-	
-	public void shoot(){
-		
+/**
+ * class Player
+ * 
+ * @author Grupp 1 - DAT055 2014
+ * @version 1.0
+ */
+public class Player
+{
+	private Battlefield battlefield;
+
+	/**
+	 * constructor
+	 */
+	public Player()
+	{
+		battlefield = new Battlefield();
 	}
 
-	public void placeShips(int ship) {
-		// TODO Auto-generated method stub
-		
+	/**
+	 * method
+	 */
+	public void placeShips()
+	{
+		;		
 	}
 
-	
+	/**
+	 * method
+	 */
+	public void attack()
+	{
+		;
+	}
+
+	/**
+	 * method
+	 */
+	public boolean hasShips()
+	{
+		return true;
+	}
+
+	/**
+	 * method
+	 */
+	public Battlefield getBattlefield()
+	{
+		return battlefield;
+	}
+
+	/**
+	 * Bomb this player's battlefield
+	 * @return true if a ship was hit
+	 *         false otherwise
+	 */
+	public boolean bomb(int x, int y)
+	{
+		if (!battlefield.isBombed(x, y)) {
+			if (battlefield.setBomb(x, y)) {
+				return true;
+			}
+		}
+		else return false;
+	}
 }
