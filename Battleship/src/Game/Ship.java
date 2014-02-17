d3a029324b6bfdf1106b832ea6e353ad500449cd
Magnus <M92@users.noreply.github.com>
2014-02-17 20:26:09 +0100

    added missing modifiers for variables and constructor

--- Battleship/src/Game/Ship.java
+++ Battleship/src/Game/Ship.java
@@ -2,28 +2,29 @@ package Game;
 
 /**
  * Handles the ships
- * @author grupp1
- *
+ * @author Group 1 - DAT055 2014
+ * @version 1.0
  */
-public class Ship {
-	String name;
-	int lenght;
-	int hit;
-	boolean isVertical;
-	boolean isSunk = false;
-	
+public class Ship
+{
+	private String name;
+	private int lenght;
+	private int hit;
+	private boolean isVertical;
+	private boolean isSunk = false;
+
 	/**
 	 * Constructor
+	 * @param lenght Length of ship
 	 * @param name Name of ship
-	 * @param lenght Lenght of ship
 	 */
-	Ship(String name, int lenght)
+	public Ship(int lenght, String name)
 	{
-		this.name = name;
 		this.lenght = lenght;
+		this.name = name;
 		hit = 0;
 	}
-	
+
 	/**
 	 * Returns true if the ship is vertical
 	 * false otherwise
@@ -32,7 +33,7 @@ public class Ship {
 	{
 		return true;
 	}*/
-	
+
 	/**
 	 * Rotates the ship 90 degrees
 	 */
@@ -49,10 +50,9 @@ public class Ship {
 	{
 		return isSunk;
 	}
-	
+
 	public void hit()
 	{
 		if(++hit == lenght) {isSunk = true;}
 	}
-	
 }
