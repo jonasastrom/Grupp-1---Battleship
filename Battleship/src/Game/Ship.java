aaa5227c73a6984149ab04fb53a3a4739b7da40a
Magnus <M92@users.noreply.github.com>
2014-02-18 10:30:18 +0100

    added methods getLenght, getName, isPlaced and setPlaced

--- Battleship/src/Game/Ship.java
+++ Battleship/src/Game/Ship.java
@@ -1,58 +1,100 @@
 package Game;
 
 /**
- * Handles the ships
+ * Handles a ship
+ * 
  * @author Group 1 - DAT055 2014
  * @version 1.0
  */
 public class Ship
 {
+	private int length;
 	private String name;
-	private int lenght;
-	private int hit;
-	private boolean isVertical;
+	private int health;
+	private boolean isVertical = false;
+	private boolean isPlaced = false;
 	private boolean isSunk = false;
 
 	/**
-	 * Constructor
-	 * @param lenght Length of ship
-	 * @param name Name of ship
+	 * Construct a ship
+	 * @param length Length of this ship
+	 * @param name Name of this ship
 	 */
-	public Ship(int lenght, String name)
+	public Ship(int length, String name)
 	{
-		this.lenght = lenght;
+		this.length = length;
 		this.name = name;
-		hit = 0;
+		health = length;
 	}
 
 	/**
-	 * Returns true if the ship is vertical
-	 * false otherwise
+	 * Return the length of this ship
 	 */
-	/*public boolean isVertical()
+	public int getLenght()
 	{
-		return true;
-	}*/
+		return length;
+	}
+	
+	/**
+	 * Return the name of this ship
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * Check the orientation of this ship
+	 * @return true if this ship is vertical,
+	 *         false if it is horizontal
+	 */
+	public boolean isVertical()
+	{
+		return isVertical;
+	}
 
 	/**
 	 * Rotates the ship 90 degrees
 	 */
-	/*public void rotate()
+	public void rotate()
 	{
-
-	}*/
+		isVertical = !isVertical;
+	}
 
 	/**
-	 * Returns true if the ship has been sun
-	 * false otherwise
+	 * Check if the ship has been placed
+	 * @return true if this ship is placed,
+	 *         false otherwise
+	 */
+	public boolean isPlaced()
+	{
+		return isPlaced;
+	}
+	
+	/**
+	 * Set this ship as placed
+	 */
+	public void setPlaced()
+	{
+		isPlaced = true;
+	}
+
+	/**
+	 * Check if the ship floats
+	 * @return true if the ship is sunk,
+	 *         false if it still floats
 	 */
 	public boolean isSunk()
 	{
 		return isSunk;
 	}
 
+	/**
+	 * Hit this ship with a bomb and
+	 * decrease it's health
+	 */
 	public void hit()
 	{
-		if(++hit == lenght) {isSunk = true;}
+		if (--health == 0) isSunk = true;
 	}
 }
