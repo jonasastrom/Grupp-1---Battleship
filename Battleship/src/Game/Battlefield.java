c4664adc11059987d7bbc34f4340b5b0d3785260
medevag <michaeledevag@gmail.com>
2014-02-19 15:43:38 +0100

    new

--- Battleship/src/Game/Battlefield.java
+++ Battleship/src/Game/Battlefield.java
@@ -19,7 +19,7 @@ public class Battlefield
 		this.id = id;
 		zones = new Zone[10][10];
 
-		for (int y = 0; y < 1; y++) {	
+		for (int y = 0; y < 10; y++) {	
 			for (int x = 0; x < 10; x++) {
 				zones[x][y] = new Zone(x,y,id);
 			}
