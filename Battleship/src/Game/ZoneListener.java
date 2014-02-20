e49bd4ec9d3ac0cf1b316cc24c82b88b99a3308a
vickiedam <Vickie_90@hotmail.com>
2014-02-20 14:15:29 +0100

    ZoneListener updated

--- Battleship/src/Game/ZoneListener.java
+++ Battleship/src/Game/ZoneListener.java
@@ -5,8 +5,8 @@ import java.util.Observable;
 public class ZoneListener extends Observable
 {
 
-	public void update(int x){
+	public void update(int x, int y, String leftRight, String state){
 		setChanged();
-		notifyObservers(new ZoneLink(x));
+		notifyObservers(new ZoneLink(x,y,leftRight, state));
 	}
 }
