cd4f29346aefc37eda43b8e0b2fa4b960da5830a
Magnus <M92@users.noreply.github.com>
2014-02-25 22:21:06 +0100

    human

--- Battleship/src/game/Human.java
+++ Battleship/src/game/Human.java
@@ -49,7 +49,6 @@ public class Human extends Player
 	public boolean placeShip(String name, int[] x, int[] y)
 	{
 		boolean error = false;
-		int length;
 		
 		Iterator<Ship> it = fleet.getShips().iterator();
 		Ship ship = null;
@@ -59,87 +58,70 @@ public class Human extends Player
 			if (ship.getName().equals(name)) break;
 		}
 		
-		length = ship.getLength();
+		int length = ship.getLength();
 
-		if (x.length != length || y.length != length) {
+		if (x.length != length || y.length != length)
 			throw new IllegalArgumentException(
-					"The provided coordinates doesn't match the length of the ship");
-		}
+					"The coordinates doesn't match the length of the ship");
 
-		/*
-		 * Checks if position 0 in x is equal to all the other positions
-		 * If so, set xEquals to false
-		 */
+		// Checks if position 0 in x is equal to all the
+		// other positions If so, set xEquals to false
 		boolean xEquals = true;
-		for (int i = 1; i < length; i++) 
-		{
+		for (int i = 1; i < length; i++) {
 			if (x[0] != x[i]) xEquals = false;
 		}
 
-		/*
-		 * Checks if position 0 in y is equal to all the other positions
-		 * If so, set yEquals to false 
-		 */
+		// Checks if position 0 in y is equal to all the
+		// other positions If so, set yEquals to false 
 		boolean yEquals = true;
-		for (int i = 1; i < length; i++) 
-		{
+		for (int i = 1; i < length; i++) {
 			if (y[0] != y[i]) yEquals = false;
 		}
 
-		if(!xEquals && !yEquals) error = true;
+		if (!xEquals && !yEquals) error = true;
 		
-		int[] c = new int[x.length];
+		int[] c = new int[length];
 		
-		/*
-		 * Checks if xEqual is true. If it is not all the entries in x[] are 
-		 * the same and the for loop is executed and fills the c[] with
-		 * all the entries in x[]
-		 */
-		if (yEquals)
-		{
-			for(int a=0; a < x.length; a++)
-			{
-				c[a] = x[a];
+		// Checks if xEqual is true. If it is not all
+		// the entries in x[] are the same and the for
+		// loop is executed and fills the c[] with
+		// all the entries in x[]
+		if (yEquals) {
+			for (int i = 0; i < length; i++) {
+				c[i] = x[i];
 			}
 		}
-		/*
-		 * Checks if yEqual is true. If it is not all the entries in y[] are 
-		 * the same and the for loop is executed and fills the c[] with
-		 * all the entries in y[]
-		 */
-		else if (xEquals){
-			for(int a=0; a < y.length; a++)
-			{
-				c[a] = y[a];
+		
+		// Checks if yEqual is true. If it is not all
+		// the entries in y[] are the same and the for
+		// loop is executed and fills the c[] with
+		// all the entries in y[]
+		else if (xEquals) {
+			for (int i = 0; i < length; i++) {
+				c[i] = y[i];
 			}
 		}
 
 		Arrays.sort(c);
 
-		/*
-		 * Checks if c of one position incremented with one is equal to 
-		 * c of the next position. If all the y-coordinates is after each other
-		 * If they are not, set error = true
-		 */
-		for (int i = 0; i < length-1; i++) 
-		{
+		// Checks if c of one position incremented with
+		// one is equal to c of the next position.
+		// If all the y-coordinates is after each
+		// other If they are not, set error = true
+		for (int i = 0; i < length-1; i++) {
 			if (c[i]+1 != c[i+1]) error = true;
 		}
 
-		/*
-		 * If error was set, go thru every coordinate and set it clear
-		 * else set ship to all the coordinates
-		 */
+		// If error was set, go thru every coordinate and
+		// set it clear else set ship to all the coordinates
 		if (error) {
-			for (int i = 0; i < length; i++) 
-			{
+			for (int i = 0; i < length; i++) {
 				battlefield.setClear(x[i],y[i]);
 			}
 			return false;
 		}
 		else {
-			for (int i = 0; i < length; i++) 
-			{
+			for (int i = 0; i < length; i++) {
 				battlefield.setShip(x[i],y[i],ship);
 			}
 			ship.setPlaced();
