0c4eb482c89250f1ff4012e4d58f2f0bed8d22b8
Magnus <M92@users.noreply.github.com>
2014-02-27 13:34:24 +0100

    started on the test client

---
+++ TestClient/src/highscore/SocketConnector.java
@@ -0,0 +1,30 @@
+package highscore;
+
+import java.util.ArrayList;
+
+public class SocketConnector
+{
+	public SocketConnector()
+	{
+
+	}
+
+	private void connect()
+	{
+		
+	}
+	
+	public ArrayList<Score> getHighScores()
+	{
+		connect();
+		
+		ArrayList<Score> highScores = null;
+		
+		return highScores;
+	}
+	
+	public void sendHighScore(Score score)
+	{
+		connect();
+	}
+}
